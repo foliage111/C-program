9abec5699e8b7c5b1dcb49bb381d4465e7182776
foliage111 <79944724+foliage111@users.noreply.github.com>
2021-06-09 09:54:09 -0500

    Add files via upload

---
+++ do while循环语句.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main(void)
+//{
+//    int i, sum;               //定义变量
+//    i = 1; sum = 0;           //变量赋值
+//    do                        //循环入口
+//    {
+//        sum = sum + i;        //累加器
+//        i = i + 1;            //累加的增量
+//    } while (i <= 100);       //循环的条件
+//    printf("sum=%d\n", sum);
+//
+//    system("pause");
+//    return 0;
+//}
